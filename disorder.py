f956ce54672935ecfc1bb83dd6857ec59f44e2da
adonvel <a.donis.vela@gmail.com>
2025-05-07 12:12:12 +0200

    Update for cluster

--- disorder.py
+++ disorder.py
@@ -7,8 +7,8 @@ import magnetic_boundaries as mb
 job = int(sys.argv[1])
 
 parameters = dict(
-    Ly = 61,
-    Lx = 61,
+    Ly = 31,
+    Lx = 31,
     theta_top = 0,
     theta_bot = 0,
     mag_field = 0.1,
@@ -22,4 +22,4 @@ spectrum = mb.tan_square_spectrum(parameters, nbands)
 
 path = '/home/donisvelaa/github/tangent-magnetic-field/data/'
 name = 'disorder_tangent'
-np.save(path+name+'_Lx'+str(parameters['Lx'])+'_Ly'+str(parameters['Ly'])+str(job), spectrum, allow_pickle=True)
+np.save(path+name+'_Lx'+str(parameters['Lx'])+'_Ly'+str(parameters['Ly'])+'_magfield'+str(parameters['mag_field'])+'_noise'+str(parameters['noise'])+str(job), spectrum, allow_pickle=True)
